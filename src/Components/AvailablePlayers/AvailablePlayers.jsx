daa9fd36023da07484a8acc69ab75afcc2afae9f
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-03-27 15:04:26 +0600

    State management & toggle functionality

--- src/Components/AvailablePlayers/AvailablePlayers.jsx
+++ src/Components/AvailablePlayers/AvailablePlayers.jsx
@@ -3,12 +3,12 @@ import React from "react";
 import Card from "../UI/Card/Card";
 
 const AvailablePlayers = ({ players }) => {
-  console.log(players);
+  // console.log(players);
   return (
     <div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         {players.map((player) => {
-          console.log(player, "player");
+          // console.log(player, "player");
           return (
             <Card player={player}></Card>
           );
--- src/Components/Players/Players.jsx
+++ src/Components/Players/Players.jsx
@@ -1,14 +1,26 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import AvailablePlayers from "../AvailablePlayers/AvailablePlayers";
+import SelectedPlayer from "../SelectPlayer/SelectedPlayer";
 
 const Players = ({ playersPromise }) => {
   //   console.log(playersPromise);
   const players = use(playersPromise);
-  console.log(players);
+  // console.log(players);
+  const [selectButton, setSelectButton] = useState("available");
+
+  // console.log(selectButton, "selectedButton");
   return (
-    <div className="mt-[88px] container mx-auto">
-      <h2>Total Players: {players.length}</h2>
-      <AvailablePlayers players={players}></AvailablePlayers>
+    <div className="mt-[88px] container mx-auto my-[60px]">
+      <div className="flex justify-between gap-4 items-center mb-5">
+        { selectButton === "available"? <h2 className="font-bold text-3xl">AVailable Players</h2>: <h2 className="font-bold text-3xl">Selected Players (2/6)</h2> }
+        <div>
+          <button onClick={()=> setSelectButton("available")} className={`btn ${selectButton === "available"? "bg-[#E7FE29]": "btn-neutral"} rounded-r-none rounded-l-xl`}>
+            Available
+          </button>
+          <button onClick={()=> setSelectButton("selected")} className={`btn ${selectButton === "selected" ? "bg-[#E7FE29]" : "btn-neutral"} rounded-l-none rounded-r-xl`}>Selected (0)</button>
+        </div>
+      </div>
+     { selectButton === "available" ? <AvailablePlayers players={players}></AvailablePlayers>: <SelectedPlayer></SelectedPlayer>}
     </div>
   );
 };
